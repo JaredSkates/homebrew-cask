07786092b7c73f602631a9880d490c5731cd1693
JaredSkates <jaredskatesz12@gmail.com>
2025-08-18 11:18:20 -0700

    Filled MetaData Co-authored-by: Sarah Wafa sarahwafa6@gmail.com

--- Casks/l/lynx-explorer.rb
+++ Casks/l/lynx-explorer.rb
@@ -2,15 +2,15 @@ cask "lynx-explorer" do
   arch arm: "arm64", intel: "x86_64"
 
   version "1.0.0"
-  sha256 arm:   "ARM_SHA256_HERE",
-         intel: "INTEL_SHA256_HERE"
+  sha256 arm:   "ebc84cfaabe7e8da40e08b9af248caf8388d50440008319074481fc0e3aa45ba",
+         intel: "3e0bef6cbe1e0877fd90e3bc2d648761458197878be7274de52141663496aa22"
 
   url "https://example.com/releases/LynxExplorer-#{arch}.app.tar.gz",
-      verified: "example.com/"
+  
 
   name "Lynx Explorer"
-  desc "File explorer that does Lynx-y things"
-  homepage "https://example.com/lynx-explorer"
+  desc "Empower the Web community and invite more to build across platforms."
+  homepage "https://lynxjs.org"
 
   # Assuming the tarball unpacks a top-level "LynxExplorer.app"
   app "LynxExplorer.app"
