440235e01f0c7ac321b1fe6f5573dd46b113edcd
JaredSkates <jaredskatesz12@gmail.com>
2025-08-19 11:12:00 -0700

    Fixed syntax error in name 3

--- Casks/l/lynx-explorer.rb
+++ Casks/l/lynx-explorer.rb
@@ -1,11 +1,11 @@
 cask "lynx-explorer" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "1.0.0"
+  version "3.3.1"
   sha256 arm:   "ebc84cfaabe7e8da40e08b9af248caf8388d50440008319074481fc0e3aa45ba",
          intel: "3e0bef6cbe1e0877fd90e3bc2d648761458197878be7274de52141663496aa22"
 
-  url "https://example.com/releases/LynxExplorer-#{arch}.app.tar.gz",
+  url "https://example.com/releases/LynxExplorer-#{arch}.app.tar.gz"
   
 
   name "LynxExplorer"
