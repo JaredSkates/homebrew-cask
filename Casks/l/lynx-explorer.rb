41279156302fa9a178cecfde9496a02aff1da19b
JaredSkates <jaredskatesz12@gmail.com>
2025-08-19 11:31:30 -0700

    Fixed url

--- Casks/l/lynx-explorer.rb
+++ Casks/l/lynx-explorer.rb
@@ -5,7 +5,7 @@ cask "lynx-explorer" do
   sha256 arm:   "ebc84cfaabe7e8da40e08b9af248caf8388d50440008319074481fc0e3aa45ba",
          intel: "3e0bef6cbe1e0877fd90e3bc2d648761458197878be7274de52141663496aa22"
 
-  url "https://example.com/releases/LynxExplorer-#{arch}.app.tar.gz"
+  url "https://github.com/lynx-family/lynx/releases/download/3.3.1/LynxExplorer-#{arch}.app.tar.gz"
   
 
   name "LynxExplorer"
